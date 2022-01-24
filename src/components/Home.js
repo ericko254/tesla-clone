5dc9fd5087b76a6bbc488c6f27a054493fa6cb0f
ericko254 <eomondi07@gmail.com>
2022-01-24 17:52:31 +0300

    mapping background image on props undone

---
+++ src/components/Home.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import Section from './Section';
+
+function Home() {
+  return <Container>
+      <Section 
+        title = "Model S"
+        description = "Order Online for touchless delivery"
+        backgroundImg = "model-s.jpg"
+        leftBtnText = "Custom order"
+        rightBtnText = "Existing Inventory"
+      />
+      <Section 
+         title = "Model Y"
+         description = "Order Online for touchless delivery"
+         backgroundImg = "model-y.jpg"
+         leftBtnText = "Custom order"
+         rightBtnText = "Existing Inventory"
+      />
+      <Section
+         title = "Model 3"
+         description = "Order Online for touchless delivery"
+         backgroundImg = "model-3.jpg"
+         leftBtnText = "Custom order"
+         rightBtnText = "Existing Inventory"
+      />
+      <Section
+         title = "Model X"
+         description = "Order Online for touchless delivery"
+         backgroundImg = "model-x.jpg"
+         leftBtnText = "Custom order"
+         rightBtnText = "Existing Inventory"
+      />
+  </Container>;
+}
+
+export default Home;
+
+
+const Container = styled.div`
+    height: 100vh;
+`
